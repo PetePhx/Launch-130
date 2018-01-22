021b91b79cb6d75c2c07f868364a974d056bd63b
PetePhx <pete0phx@yandex.com>
2018-01-22 12:40:05 -0700

    solve medium 2 set exercise #2

---
+++ exercises/medium_2_testing/02_setup_the_test_class_cash_register.rb
@@ -0,0 +1,23 @@
+=begin
+
+Setup the Test Class - Cash Register
+
+Let's start things from the ground up. We want to make a simple test suite for
+our CashRegister class. Setup the initial testing file. You don't have to have
+any tests in your test file. For this exercise, write everything you would need
+to start testing CashRegister, excluding the tests themselves (necessary
+requires, test class, etc.).
+
+=end
+
+# cash_register_test.rb
+
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+require_relative 'cash_register'
+require_relative 'transaction'
+
+class CashRegisterTest < MiniTest::Test
+end
