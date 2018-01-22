4d4cb8fe91b70ae996ccb47f1d7930b93ad45d2d
PetePhx <pete0phx@yandex.com>
2018-01-22 12:33:44 -0700

    medium set 2 exercise #1

---
+++ exercises/medium_2_testing/01_classes_to_test_cash_register_and_transaction.rb
@@ -0,0 +1,12 @@
+=begin
+
+Classes to Test - Cash Register and Transaction
+
+These will be the classes you will be testing throughout the remaining
+exercises. You'll want to create two new files, one for each of the two classes
+listed below. You'll need this code for the remaining exercises of this set.
+
+=end
+
+# cash_register.rb
+# transaction.rb
